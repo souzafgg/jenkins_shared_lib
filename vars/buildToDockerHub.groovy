2f63843b9d0ea6838354a66f4cf7902c53eaec01
Your Name <you@example.com>
2023-05-02 02:47:09 -0300

    Added push

--- vars/buildToDockerHub.groovy
+++ vars/buildToDockerHub.groovy
@@ -1,5 +1,9 @@
-def call() {
-    docker.withRegistry('https://registry.hub.docker.com', 'docker-creds')
-    dockerapp.push('latest')            
-    dockerapp.push("${env.BUILD_ID}")
+def call(String dockerUser, String imageTag, String project){
+withCredentials([usernamePassword(credentialsId: 'docker-creds', passwordVariable: 'dockerPassword', usernameVariable: 'dockerUsername')]) {
+    sh """
+    docker login -u ${dockerUsername} -p ${dockerPassword}
+    docker push ${dockerUser}/${project}:${imageTag}
+    """
 }
+}
+
