7d8b303027fb562fcd2f58f644c8d93a905cbe76
Your Name <you@example.com>
2023-05-02 10:58:49 -0300

    Trying a fix

--- vars/runAppDocker.groovy
+++ vars/runAppDocker.groovy
@@ -1,3 +1,3 @@
 def call(project, dockerUser, imageTag) {
-  sh 'docker run -d -p 10000:8080 --name ${project} --rm ${dockerUser}/${project}:${imageTag}'
+  sh 'docker run -d --rm -p 10000:8080 --name ${project} ${dockerUser}/${project}:${imageTag}'
 }
